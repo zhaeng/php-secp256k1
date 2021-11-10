9c4e167cd76344875667fa5d0a9dcd93a5c57bb3
zhaeng <zhangf@disanbo.com>
2021-11-10 09:59:13 +0800

    change HexSignatureSerializer::serialize()

--- src/Serializer/HexSignatureSerializer.php
+++ src/Serializer/HexSignatureSerializer.php
@@ -11,8 +11,23 @@ class HexSignatureSerializer
     public function serialize(SignatureInterface $signature): string {
         $r = $signature->getR();
         $s = $signature->getS();
+        $rr = str_pad(gmp_strval($r,16), 64, '0', STR_PAD_LEFT);
+        if ((hexdec(substr($rr,0,2)) & 0x80) != 0) {
+            $rr = '00' . $rr;
+        }
+        $ss = str_pad(gmp_strval($s,16), 64, '0', STR_PAD_LEFT);
+        if ((hexdec(substr($ss,0,2)) & 0x80) != 0) {
+            $ss = '00' . $ss;
+        }
 
-        return str_pad(gmp_strval($r, 16), 64, '0', STR_PAD_LEFT) . str_pad(gmp_strval($s, 16), 64, '0', STR_PAD_LEFT);
+        $signature = '02' . dechex(strlen(hex2bin($rr))) . $rr . '02' . dechex(strlen(hex2bin($ss))) . $ss;
+
+        return '30' . dechex(strlen(hex2bin($signature))) . $signature;
+        // change by zhaeng 2021-11-10
+        //$r = $signature->getR();
+        //$s = $signature->getS();
+
+        //return str_pad(gmp_strval($r, 16), 64, '0', STR_PAD_LEFT) . str_pad(gmp_strval($s, 16), 64, '0', STR_PAD_LEFT);
     }
 
     public function parse(string $binary): SignatureInterface {
